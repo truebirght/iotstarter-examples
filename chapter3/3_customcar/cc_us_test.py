915a9ceafb78cfd5dce4baefa4f55e77827ee526
Jin Hyun Park <pjh0410@gmail.com>
2019-02-27 19:40:44 +0900

    3장 블루투스를 제외한 종합예제 완성

--- chapter3/3_customcar/cc_us_test.py
+++ chapter3/3_customcar/cc_us_test.py
@@ -6,6 +6,6 @@ echo_pin = 23
 
 while True:
 	value = sensor.Measurement(trig_pin, echo_pin)
-	raw_measurement = value.raw_distance()
+	raw_measurement = value.raw_distance(sample_size = 11, sample_wait= 0.2)
 	metric_distance = value.distance_metric(raw_measurement)
 	print(" [{}] {}cm".format(datetime.datetime.now().strftime("%H:%M:%S"),metric_distance))
